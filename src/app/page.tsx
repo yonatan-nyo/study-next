62a68a1fd3e9d30d826fb64c4e144d94813396c6
yonatan-nyo <yonatan.njoto@gmail.com>
2023-08-21 22:29:23 +0700

    added card to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,51 @@
-import Image from 'next/image'
+import Card from "@/components/Card";
 
 export default function Home() {
+  const data = [
+    {
+      name: "haha",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt augue. In in nunc quis odio lacinia posuere. Donec quis consectetur ante. Praesent ullamcorper, lacus sed auctor pellentesque, nisi nulla tristique libero, eget facilisis lorem mauris porta ipsum. Aenean ut leo arcu. In hac habitasse platea dictumst. Nam aliquet sodales est sit amet tincidunt. Proin non est a mi fermentum interdum quis ut justo. Sed malesuada lorem aliquam erat vestibulum rhoncus. Etiam cursus, dolor quis aliquet tempor, eros eros commodo nunc, nec vulputate mauris nibh sit amet eros. Donec cursus convallis dolor, a rutrum purus suscipit sed. Nam hendrerit dolor sit amet diam faucibus consequat. Quisque mollis dignissim vestibulum. Sed condimentum vehicula enim, eget hendrerit sapien molestie sit amet.\n Aenean porta ut leo ut feugiat. Vestibulum id dolor scelerisque, pretium ex laoreet, accumsan sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vulputate eu nisl id sollicitudin. Pellentesque quis augue pellentesque, volutpat augue sit amet, sagittis quam. Etiam quam mauris, suscipit eu est dapibus, mattis posuere massa. Aenean suscipit lorem non egestas faucibus. Duis interdum purus tempor nisl blandit, sit amet volutpat quam ullamcorper. Aliquam at magna urna. Curabitur cursus posuere laoreet. Aenean a mi volutpat, auctor augue molestie, dictum urna.",
+    },
+    {
+      name: "1",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt augue. In in nunc quis odio lacinia posuere. Donec quis consectetur ante. Praesent ullamcorper, lacus sed auctor pellentesque, nisi nulla tristique libero, eget facilisis lorem mauris porta ipsum. Aenean ut leo arcu. In hac habitasse platea dictumst. Nam aliquet sodales est sit amet tincidunt. Proin non est a mi fermentum interdum quis ut justo. Sed malesuada lorem aliquam erat vestibulum rhoncus. Etiam cursus, dolor quis aliquet tempor, eros eros commodo nunc, nec vulputate mauris nibh sit amet eros. Donec cursus convallis dolor, a rutrum purus suscipit sed. Nam hendrerit dolor sit amet diam faucibus consequat. Quisque mollis dignissim vestibulum. Sed condimentum vehicula enim, eget hendrerit sapien molestie sit amet.\n Aenean porta ut leo ut feugiat. Vestibulum id dolor scelerisque, pretium ex laoreet, accumsan sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vulputate eu nisl id sollicitudin. Pellentesque quis augue pellentesque, volutpat augue sit amet, sagittis quam. Etiam quam mauris, suscipit eu est dapibus, mattis posuere massa. Aenean suscipit lorem non egestas faucibus. Duis interdum purus tempor nisl blandit, sit amet volutpat quam ullamcorper. Aliquam at magna urna. Curabitur cursus posuere laoreet. Aenean a mi volutpat, auctor augue molestie, dictum urna.",
+    },
+    {
+      name: "2",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt augue. In in nunc quis odio lacinia posuere. Donec quis consectetur ante. Praesent ullamcorper, lacus sed auctor pellentesque, nisi nulla tristique libero, eget facilisis lorem mauris porta ipsum. Aenean ut leo arcu. In hac habitasse platea dictumst. Nam aliquet sodales est sit amet tincidunt. Proin non est a mi fermentum interdum quis ut justo. Sed malesuada lorem aliquam erat vestibulum rhoncus. Etiam cursus, dolor quis aliquet tempor, eros eros commodo nunc, nec vulputate mauris nibh sit amet eros. Donec cursus convallis dolor, a rutrum purus suscipit sed. Nam hendrerit dolor sit amet diam faucibus consequat. Quisque mollis dignissim vestibulum. Sed condimentum vehicula enim, eget hendrerit sapien molestie sit amet.\n Aenean porta ut leo ut feugiat. Vestibulum id dolor scelerisque, pretium ex laoreet, accumsan sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vulputate eu nisl id sollicitudin. Pellentesque quis augue pellentesque, volutpat augue sit amet, sagittis quam. Etiam quam mauris, suscipit eu est dapibus, mattis posuere massa. Aenean suscipit lorem non egestas faucibus. Duis interdum purus tempor nisl blandit, sit amet volutpat quam ullamcorper. Aliquam at magna urna. Curabitur cursus posuere laoreet. Aenean a mi volutpat, auctor augue molestie, dictum urna.",
+    },
+    {
+      name: "3",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt augue. In in nunc quis odio lacinia posuere. Donec quis consectetur ante. Praesent ullamcorper, lacus sed auctor pellentesque, nisi nulla tristique libero, eget facilisis lorem mauris porta ipsum. Aenean ut leo arcu. In hac habitasse platea dictumst. Nam aliquet sodales est sit amet tincidunt. Proin non est a mi fermentum interdum quis ut justo. Sed malesuada lorem aliquam erat vestibulum rhoncus. Etiam cursus, dolor quis aliquet tempor, eros eros commodo nunc, nec vulputate mauris nibh sit amet eros. Donec cursus convallis dolor, a rutrum purus suscipit sed. Nam hendrerit dolor sit amet diam faucibus consequat. Quisque mollis dignissim vestibulum. Sed condimentum vehicula enim, eget hendrerit sapien molestie sit amet.\n Aenean porta ut leo ut feugiat. Vestibulum id dolor scelerisque, pretium ex laoreet, accumsan sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vulputate eu nisl id sollicitudin. Pellentesque quis augue pellentesque, volutpat augue sit amet, sagittis quam. Etiam quam mauris, suscipit eu est dapibus, mattis posuere massa. Aenean suscipit lorem non egestas faucibus. Duis interdum purus tempor nisl blandit, sit amet volutpat quam ullamcorper. Aliquam at magna urna. Curabitur cursus posuere laoreet. Aenean a mi volutpat, auctor augue molestie, dictum urna.",
+    },
+    {
+      name: "4",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt augue. In in nunc quis odio lacinia posuere. Donec quis consectetur ante. Praesent ullamcorper, lacus sed auctor pellentesque, nisi nulla tristique libero, eget facilisis lorem mauris porta ipsum. Aenean ut leo arcu. In hac habitasse platea dictumst. Nam aliquet sodales est sit amet tincidunt. Proin non est a mi fermentum interdum quis ut justo. Sed malesuada lorem aliquam erat vestibulum rhoncus. Etiam cursus, dolor quis aliquet tempor, eros eros commodo nunc, nec vulputate mauris nibh sit amet eros. Donec cursus convallis dolor, a rutrum purus suscipit sed. Nam hendrerit dolor sit amet diam faucibus consequat. Quisque mollis dignissim vestibulum. Sed condimentum vehicula enim, eget hendrerit sapien molestie sit amet.\n Aenean porta ut leo ut feugiat. Vestibulum id dolor scelerisque, pretium ex laoreet, accumsan sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vulputate eu nisl id sollicitudin. Pellentesque quis augue pellentesque, volutpat augue sit amet, sagittis quam. Etiam quam mauris, suscipit eu est dapibus, mattis posuere massa. Aenean suscipit lorem non egestas faucibus. Duis interdum purus tempor nisl blandit, sit amet volutpat quam ullamcorper. Aliquam at magna urna. Curabitur cursus posuere laoreet. Aenean a mi volutpat, auctor augue molestie, dictum urna.",
+    },
+    {
+      name: "5",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt augue. In in nunc quis odio lacinia posuere. Donec quis consectetur ante. Praesent ullamcorper, lacus sed auctor pellentesque, nisi nulla tristique libero, eget facilisis lorem mauris porta ipsum. Aenean ut leo arcu. In hac habitasse platea dictumst. Nam aliquet sodales est sit amet tincidunt. Proin non est a mi fermentum interdum quis ut justo. Sed malesuada lorem aliquam erat vestibulum rhoncus. Etiam cursus, dolor quis aliquet tempor, eros eros commodo nunc, nec vulputate mauris nibh sit amet eros. Donec cursus convallis dolor, a rutrum purus suscipit sed. Nam hendrerit dolor sit amet diam faucibus consequat. Quisque mollis dignissim vestibulum. Sed condimentum vehicula enim, eget hendrerit sapien molestie sit amet.\n Aenean porta ut leo ut feugiat. Vestibulum id dolor scelerisque, pretium ex laoreet, accumsan sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vulputate eu nisl id sollicitudin. Pellentesque quis augue pellentesque, volutpat augue sit amet, sagittis quam. Etiam quam mauris, suscipit eu est dapibus, mattis posuere massa. Aenean suscipit lorem non egestas faucibus. Duis interdum purus tempor nisl blandit, sit amet volutpat quam ullamcorper. Aliquam at magna urna. Curabitur cursus posuere laoreet. Aenean a mi volutpat, auctor augue molestie, dictum urna.",
+    },
+    {
+      name: "6",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tincidunt augue. In in nunc quis odio lacinia posuere. Donec quis consectetur ante. Praesent ullamcorper, lacus sed auctor pellentesque, nisi nulla tristique libero, eget facilisis lorem mauris porta ipsum. Aenean ut leo arcu. In hac habitasse platea dictumst. Nam aliquet sodales est sit amet tincidunt. Proin non est a mi fermentum interdum quis ut justo. Sed malesuada lorem aliquam erat vestibulum rhoncus. Etiam cursus, dolor quis aliquet tempor, eros eros commodo nunc, nec vulputate mauris nibh sit amet eros. Donec cursus convallis dolor, a rutrum purus suscipit sed. Nam hendrerit dolor sit amet diam faucibus consequat. Quisque mollis dignissim vestibulum. Sed condimentum vehicula enim, eget hendrerit sapien molestie sit amet.\n Aenean porta ut leo ut feugiat. Vestibulum id dolor scelerisque, pretium ex laoreet, accumsan sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vulputate eu nisl id sollicitudin. Pellentesque quis augue pellentesque, volutpat augue sit amet, sagittis quam. Etiam quam mauris, suscipit eu est dapibus, mattis posuere massa. Aenean suscipit lorem non egestas faucibus. Duis interdum purus tempor nisl blandit, sit amet volutpat quam ullamcorper. Aliquam at magna urna. Curabitur cursus posuere laoreet. Aenean a mi volutpat, auctor augue molestie, dictum urna.",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="max-w-[1240px] p-4 mx-auto">
+      <div className="flex flex-wrap gap-4">
+        {data?.map((el, i) => (
+          <Card key={i} name={el.name} description={el.description} />
+        ))}
       </div>
     </main>
-  )
+  );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface CardProps {
+  name: string;
+  description: string;
+}
+
+const Card: React.FC<CardProps> = ({ name, description }) => {
+  return (
+    <div className="flex flex-col p-4 justify-center bg-blue-200 flex-grow-0 rounded-xl shadow-xl border-4 hover:bg-blue-300 hover:scale-105 ease-in duration-100 max-w-[49%]">
+      <h2 className="text-2xl uppercase mb-10">{name}</h2>
+      <p className="whitespace-pre-line">{description}</p>
+    </div>
+  );
+};
+
+export default Card;
